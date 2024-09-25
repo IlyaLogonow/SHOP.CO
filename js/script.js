41fae8cbc7cbdf8bb4daa88596cfffbed9feb1eb
ninjoix <ninjoix@gmail.com>
2024-09-25 14:01:45 +0300

    Finished the 'hero' block, also finished the adaptive.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burger-menu');
     const mainNav = document.querySelector('.main-nav');
+    const closeIcon = document.querySelector('.close-icon');
+    const headerPromo = document.querySelector('.header-promo');
 
     burgerMenu.addEventListener('click', function() {
         burgerMenu.classList.toggle('active');
         mainNav.classList.toggle('active');
+    });
+
+    closeIcon.addEventListener('click', function() {
+        headerPromo.classList.add('hidden');
     });
 });
 /*header-promo-end*/
