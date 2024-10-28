3af3251e09f58d8c10a8e145a61213cb8e4b3c91
ninjoix <ninjoix@gmail.com>
2024-10-28 23:58:17 +0300

    I made a slider

--- js/script.js
+++ js/script.js
@@ -61,6 +61,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 /*new-arrivals-end*/
+
+var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 /*MAIN-END*/
 
  
